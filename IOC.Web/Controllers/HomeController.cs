d8b3165ab7db306492da0b70ee4b042aa37011d0
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-11-11 17:17:46 +0300

    IScopedDateService

--- IOC.Web/Controllers/HomeController.cs
+++ IOC.Web/Controllers/HomeController.cs
@@ -7,16 +7,25 @@ namespace IOC.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ISingletonDateService _singletonDateService;
+        private readonly IScopedDateService _scopedDateService;
 
-        public HomeController(ISingletonDateService singletonDateService)
+        public HomeController(ISingletonDateService singletonDateService, IScopedDateService scopedDateService)
         {
             _singletonDateService = singletonDateService;
+            _scopedDateService = scopedDateService;
         }
 
-        public IActionResult Index([FromServices]ISingletonDateService _singletonDateService2)
+        public IActionResult Index([FromServices]ISingletonDateService _singletonDateService2, [FromServices]IScopedDateService _scopedDateService2)
         {
+            // Singleton
             ViewBag.SingletonTime1 = _singletonDateService.GetCurrentDate.ToString();
             ViewBag.SingletonTime2 = _singletonDateService2.GetCurrentDate.ToString();
+            
+            // Scoped
+            ViewBag.ScopedTime1 = _scopedDateService.GetCurrentDate.TimeOfDay.ToString();
+            ViewBag.ScopedTime2 = _scopedDateService2.GetCurrentDate.TimeOfDay.ToString();
+            
+            
             return View();
         }
 
